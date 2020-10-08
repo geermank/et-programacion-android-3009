87979f971f763e4ba581924f907c7ac195c378c1
German Krajcar <german.krajcar@mercadolibre.com>
2020-10-07 22:02:29 -0300

    Clase 2, parte 2

--- app/src/main/java/com/german/tareasapp/WelcomeActivity.java
+++ app/src/main/java/com/german/tareasapp/WelcomeActivity.java
@@ -1,22 +1,28 @@
 package com.german.tareasapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
-
-    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        btnLogin = findViewById(R.id.btn_login);
+        TextView tvAppName = findViewById(R.id.tv_app_name);
+        // getString(R.string.mensaje_bienvenida)
+        tvAppName.setText(R.string.mensaje_bienvenida);
+
+        tvAppName.setTextColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+
+        Button btnLogin = findViewById(R.id.btn_login);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
