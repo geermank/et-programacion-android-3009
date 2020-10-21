a0dff0df46f75b169cedf3e71b3acb7fa217a6da
geermank <germankrajcar@gmail.com>
2020-10-21 20:17:55 -0300

    Clase 4, parte 1

--- app/src/main/java/com/german/tareasapp/MainActivity.java
+++ app/src/main/java/com/german/tareasapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.german.tareasapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,5 +23,31 @@ public class MainActivity extends AppCompatActivity {
         //int age = loginIntent.getIntExtra("userAge", -1);
 
         Toast.makeText(this, email + " " + password, Toast.LENGTH_SHORT).show();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Mis tareas");
+        }
+        */
+        getSupportActionBar().setTitle("Mis tareas");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_add_task) {
+            Toast.makeText(this, "Agregando tarea", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId() == R.id.action_logout) {
+            Toast.makeText(this, "Cerrando sesión", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
